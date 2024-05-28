e5cf6855b9075d46ed8c1114b63d783e5a270d45
Umairmughal92 <umair0337782@gmail.com>
2024-05-28 01:04:54 -0700

    adding a nuber guessing game

---
+++ main.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n welcome to umairs-cli Number Guessing Game");
+
+const randomnumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name:"guessednumber",
+        type:"number",
+        message:"Enter your guess number(Number list from 1 to 5 ):"
+    }]
+)
+
+if(answer.guessednumber === randomnumber){
+    console.log("you guessed a correct number");
+}
+else{
+    console.log("you guessed awrong number");
+}
+
+
+
+
+
+
